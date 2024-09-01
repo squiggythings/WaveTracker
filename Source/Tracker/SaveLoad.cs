e645710909c79e0db97e2b4c0eb9fe3a30e91107
squiggy <46301527+squiggythings@users.noreply.github.com>
2024-08-31 23:13:15 -0400

    Revert "Makes sample export use sample name as default filename"
    This reverts commit da1f11617600069e410d0a1be8216d94c1cd93b7.

--- Source/Tracker/SaveLoad.cs
+++ Source/Tracker/SaveLoad.cs
@@ -496,13 +496,8 @@ namespace WaveTracker {
                     saveFileDialog.DefaultExt = "wav";
                     saveFileDialog.InitialDirectory = ReadPath("sample_export", Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments));
                     saveFileDialog.OverwritePrompt = true;
-                    if (((SampleInstrument)App.InstrumentBank.GetCurrentInstrument).sample.name != null) {
-                        saveFileDialog.FileName = ((SampleInstrument)App.InstrumentBank.GetCurrentInstrument).sample.name;
-                    }
-                    else {
-                        saveFileDialog.FileName = App.InstrumentBank.GetCurrentInstrument.name;
-                    }
-                    saveFileDialog.Title = "Export sample...";
+                    saveFileDialog.FileName = App.InstrumentBank.GetCurrentInstrument.name;
+                    saveFileDialog.Title = "Export .wav";
                     saveFileDialog.Filter = "Waveform Audio File Format (*.wav)|*.wav|All files (*.*)|*.*";
                     saveFileDialog.AddExtension = true;
                     saveFileDialog.CheckPathExists = true;
