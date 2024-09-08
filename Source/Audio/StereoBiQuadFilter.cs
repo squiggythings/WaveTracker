b05a25955689e2f507360df1a539daa31ccb6d87
Speykious <speykious@gmail.com>
2024-09-08 17:21:42 +0200

    Revive StereoBiQuadFilter

--- Source/Audio/StereoBiQuadFilter.cs
+++ Source/Audio/StereoBiQuadFilter.cs
@@ -1,29 +1,31 @@
 ﻿
+using WaveTracker.Audio.NAudio.Dsp;
+
 namespace WaveTracker.Audio {
     public class StereoBiQuadFilter {
-        // private BiQuadFilter filterL;
-        // private BiQuadFilter filterR;
+        private BiQuadFilter filterL;
+        private BiQuadFilter filterR;
 
         public StereoBiQuadFilter() {
-            // filterL = BiQuadFilter.LowPassFilter(AudioEngine.TrueSampleRate, AudioEngine.SampleRate / 2f, 1);
-            // filterR = BiQuadFilter.LowPassFilter(AudioEngine.TrueSampleRate, AudioEngine.SampleRate / 2f, 1);
+            filterL = BiQuadFilter.LowPassFilter(AudioEngine.TrueSampleRate, AudioEngine.SampleRate / 2f, 1);
+            filterR = BiQuadFilter.LowPassFilter(AudioEngine.TrueSampleRate, AudioEngine.SampleRate / 2f, 1);
         }
 
         public void SetLowpassFilter(float sampleRate, float cutoffFrequency, float q) {
-            // filterL.SetLowPassFilter(sampleRate, cutoffFrequency, q);
-            // filterR.SetLowPassFilter(sampleRate, cutoffFrequency, q);
+            filterL.SetLowPassFilter(sampleRate, cutoffFrequency, q);
+            filterR.SetLowPassFilter(sampleRate, cutoffFrequency, q);
         }
 
         public void Transform(float inputL, float inputR, out float outputL, out float outputR) {
-            outputL = inputL; //filterL.Transform(inputL);
-            outputR = inputR; //filterR.Transform(inputR);
+            outputL = filterL.Transform(inputL);
+            outputR = filterR.Transform(inputR);
         }
 
         public float TransformL(float input) {
-            return input; // filterL.Transform(input);
+            return filterL.Transform(input);
         }
         public float TransformR(float input) {
-            return input; // filterR.Transform(input);
+            return filterR.Transform(input);
         }
     }
 }
