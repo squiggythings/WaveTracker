cbe789c8df37c1d7b003bde052023fa1b0048aab
squiggy <46301527+squiggythings@users.noreply.github.com>
2024-10-04 16:51:32 -0400

    Only autosave if new changes were made since last autosave

--- Source/App.cs
+++ Source/App.cs
@@ -257,6 +257,8 @@ namespace WaveTracker {
                 new MenuOption("Open manual...", OpenHelp),
                 new MenuOption("Effect list...", OpenEffectList),
                 null,
+                new SubMenu("Recover files...", SaveLoad.CreateAutosavesMenu()),
+                null,
                 new MenuOption("Reset audio", ResetAudio),
             ]));
 
